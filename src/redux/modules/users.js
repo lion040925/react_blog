6f0bb8af90eea08544896a585f04455a4363e8ab
sangmin <mis93ysm@naver.com>
2019-01-16 01:52:43 +0900

    first

---
+++ src/redux/modules/users.js
@@ -0,0 +1,24 @@
+// import
+
+// actions
+
+// action creators
+
+// initial state
+const initialState = {
+    isLoggedIn : localStorage.getItem("jwt") || false
+}
+
+// reducer
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+// reducer function
+
+// exports
+
+// reducer export
